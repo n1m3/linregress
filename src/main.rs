fe2a733ffc870e364faad734bcd68d51f9b6e6e8
Nils Mehrtens <nils@hasinila.de>
2019-02-14 21:13:18 +0100

    Implement OLS regression with QR method

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![warn(rust_2018_idioms)]
-use failure::Error;
-use nalgebra::{RowDVector, DMatrix};
-
+use failure::{bail, err_msg, Error};
+use nalgebra::{DMatrix, RowDVector};
 
 fn main() {
     let inputs = RowDVector::from_vec(vec![1., 3., 4., 5., 2., 3., 4.]);
+    #[rustfmt::skip]
     let outputs = DMatrix::from_vec(7,2,
                                     vec![
                                     1., 1., 1., 1., 1., 1., 1.,
                                     1., 2., 3., 4., 5., 6., 7.]);
+    let (slope, intercept) = ols_qr(&inputs, &outputs).expect("Solving failed!");
+    println!("Slope {}\nIntercept{}", slope, intercept);
+}
+fn ols_qr(inputs: &RowDVector<f64>, outputs: &DMatrix<f64>) -> Result<(f64, f64), Error> {
     let qr = outputs.to_owned().qr();
     let (q, r) = (qr.q(), qr.r());
-    let normalized_cov_params = &r.tr_mul(&r).pseudo_inverse(0.);
-    let singular_values = q.to_owned().svd(false, false);
+    let _normalized_cov_params = &r.tr_mul(&r).pseudo_inverse(0.);
+    let _singular_values = q.to_owned().svd(false, false);
     let effects = get_sum_of_products(&q.transpose(), &inputs);
-    dbg!(effects);
+    let result = r.qr().solve(&effects).ok_or(err_msg("Solving failed"))?;
+    if result.len() < 2 {
+        bail!("Invalid result matrix");
+    }
+    let slope = result[0];
+    let intercept = result[1];
+    Ok((slope, intercept))
 }
-fn get_sum_of_products(matrix: &DMatrix<f64>, vector: &RowDVector<f64>) -> Result<DMatrix<f64>, Error> {
+fn get_sum_of_products(matrix: &DMatrix<f64>, vector: &RowDVector<f64>) -> DMatrix<f64> {
     let mut v: Vec<f64> = Vec::new();
     for row_index in 0..matrix.nrows() {
         let row = matrix.row(row_index);
@@ -26,6 +36,5 @@ fn get_sum_of_products(matrix: &DMatrix<f64>, vector: &RowDVector<f64>) -> Resul
         }
         v.push(sum);
     }
-    let result: DMatrix<f64> = DMatrix::from_vec(matrix.nrows(), 1, v);
-    Ok(result)
+    DMatrix::from_vec(matrix.nrows(), 1, v)
 }
