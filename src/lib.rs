d672f16d92b0d3332f36888f4421b2e6d5e76c76
Nils Mehrtens <nils@hasinila.de>
2019-02-16 00:29:02 +0100

    Delete now obsolete code and tests

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@ impl FormulaRegressionBuilder {
         //
         let output_matrix = DMatrix::from_vec(input_vector.len(), outputs.len() + 1, output_matrix);
         let (model_parameter, singular_values, normalized_cov_params) =
-            Self::fit_ols_pinv(&input_vector, &output_matrix)?;
+            fit_ols_pinv(&input_vector, &output_matrix)?;
         RegressionModel::try_from_regression_parameters(
             &input_vector,
             &output_matrix,
@@ -89,27 +89,6 @@ impl FormulaRegressionBuilder {
             data,
             formula,
         )
-    }
-    // XXX move to Regression result as with_ols_...
-    /// Performs ordinary least squared linear regression using the pseudo inverse method to solve
-    /// the linear system.
-    ///
-    /// Returns a tuple of the form `(model_parameter, singular_values, normalized_cov_params)` as `Result`.
-    fn fit_ols_pinv(
-        inputs: &RowDVector<f64>,
-        outputs: &DMatrix<f64>,
-    ) -> Result<(DMatrix<f64>, DVector<f64>, DMatrix<f64>), Error> {
-        let singular_values = &outputs.to_owned().svd(false, false).singular_values;
-        let pinv = outputs
-            .to_owned()
-            .pseudo_inverse(0.)
-            .map_err(|_| err_msg("Taking the pinv of the output matrix failed"))?;
-        let normalized_cov_params = &pinv * &pinv.transpose();
-        let params = get_sum_of_products(&pinv, &inputs);
-        if params.len() < 2 {
-            bail!("Invalid parameter matrix");
-        }
-        Ok((params, singular_values.to_owned(), normalized_cov_params))
     }
 }
 
@@ -147,7 +126,7 @@ impl RegressionModel {
         let n = inputs.ncols();
         let scale = ssr / ((n - p) as f64);
         let cov_params = normalized_cov_params.to_owned() * scale;
-        let se = get_bse_from_cov_params(&cov_params)?;
+        let se = get_se_from_cov_params(&cov_params)?;
         let centered_input_matrix = subtract_value_from_matrix(&input_matrix, input_matrix.mean());
         let centered_tss = &centered_input_matrix.dot(&centered_input_matrix);
         let rsquared = 1. - (ssr / centered_tss);
@@ -181,17 +160,15 @@ impl RegressionModel {
         })
     }
 }
-
-/// Performs a linear regression.
+/// Performs ordinary least squared linear regression using the pseudo inverse method to solve
+/// the linear system.
 ///
-/// Peforms a ordinary least squared linear regression using the pseudo inverse method to solve the linear system.
-/// This method supports multiple linear regression.
-/// If successful it returns a `Vec` of the form `vec![intercept, slope1, slope2, ...]`.
-///
-pub fn ols_pinv(inputs: &RowDVector<f64>, outputs: &DMatrix<f64>) -> Result<Vec<f64>, Error> {
+/// Returns a tuple of the form `(model_parameter, singular_values, normalized_cov_params)` as `Result`.
+fn fit_ols_pinv(
+    inputs: &RowDVector<f64>,
+    outputs: &DMatrix<f64>,
+) -> Result<(DMatrix<f64>, DVector<f64>, DMatrix<f64>), Error> {
     let singular_values = &outputs.to_owned().svd(false, false).singular_values;
-    let diag = DMatrix::from_diagonal(&singular_values);
-    let rank = &diag.rank(0.0);
     let pinv = outputs
         .to_owned()
         .pseudo_inverse(0.)
@@ -201,34 +178,9 @@ pub fn ols_pinv(inputs: &RowDVector<f64>, outputs: &DMatrix<f64>) -> Result<Vec<
     if params.len() < 2 {
         bail!("Invalid parameter matrix");
     }
-    let result: Vec<f64> = params.iter().cloned().collect();
-    let input_vec: Vec<_> = inputs.iter().cloned().collect();
-    let input_matrix = DMatrix::from_vec(inputs.len(), 1, input_vec);
-    let residuals = &input_matrix - (outputs * params.to_owned());
-    let ssr = residuals.dot(&residuals);
-    let p = outputs.ncols() - 1;
-    let n = inputs.ncols();
-    let scale = ssr / ((n - p) as f64);
-    let cov_params = normalized_cov_params.to_owned() * scale;
-    let bse = get_bse_from_cov_params(&cov_params)?;
-    let centered_input_matrix = subtract_value_from_matrix(&input_matrix, input_matrix.mean());
-    let centered_tss = &centered_input_matrix.dot(&centered_input_matrix);
-    let rsquared = 1. - (ssr / centered_tss);
-    let df_resid = n - rank;
-    let _rsquared_adj = 1. - ((n - 1) as f64 / df_resid as f64 * (1. - rsquared));
-    let tvalues: Vec<_> = matrix_as_vec(&params)
-        .iter()
-        .zip(matrix_as_vec(&bse))
-        .map(|(x, y)| x / y)
-        .collect();
-    let students_t = StudentsT::new(0.0, 1.0, df_resid as f64)?;
-    let _pvalues: Vec<_> = tvalues
-        .iter()
-        .cloned()
-        .map(|x| (1. - students_t.cdf(x)) * 2.)
-        .collect();
-    Ok(result)
+    Ok((params, singular_values.to_owned(), normalized_cov_params))
 }
+/// Transforms a matrix into a flat Vec.
 fn matrix_as_vec(matrix: &DMatrix<f64>) -> Vec<f64> {
     let mut vector = Vec::new();
     for row_index in 0..matrix.nrows() {
@@ -239,6 +191,7 @@ fn matrix_as_vec(matrix: &DMatrix<f64>) -> Vec<f64> {
     }
     vector
 }
+/// Subtracts `value` from all fields in `matrix` and returns the resulting new matrix.
 fn subtract_value_from_matrix(matrix: &DMatrix<f64>, value: f64) -> DMatrix<f64> {
     let mut v = Vec::new();
     for row_index in 0..matrix.nrows() {
@@ -249,7 +202,8 @@ fn subtract_value_from_matrix(matrix: &DMatrix<f64>, value: f64) -> DMatrix<f64>
     }
     DMatrix::from_vec(matrix.nrows(), matrix.ncols(), v)
 }
-fn get_bse_from_cov_params(matrix: &DMatrix<f64>) -> Result<DMatrix<f64>, Error> {
+/// Calculates the standard errors given a models covariate parameters
+fn get_se_from_cov_params(matrix: &DMatrix<f64>) -> Result<DMatrix<f64>, Error> {
     let mut v = Vec::new();
     for row_index in 0..matrix.ncols() {
         let row = matrix.row(row_index);
@@ -259,30 +213,6 @@ fn get_bse_from_cov_params(matrix: &DMatrix<f64>) -> Result<DMatrix<f64>, Error>
         v.push(row[row_index].sqrt());
     }
     Ok(DMatrix::from_vec(matrix.ncols(), 1, v))
-}
-
-/// Performs a linear regression.
-///
-/// Peforms a ordinary least squared linear regression using the QR method to solve the linear system.
-/// This method does not support multile linear regression.
-/// If successful it returns a tuple of the form `(intercept, slope)`.
-///
-pub fn ols_qr(inputs: &RowDVector<f64>, outputs: &DMatrix<f64>) -> Result<(f64, f64), Error> {
-    let qr = outputs.to_owned().qr();
-    let (q, r) = (qr.q(), qr.r());
-    let _normalized_cov_params = &r.tr_mul(&r).pseudo_inverse(0.);
-    let _singular_values = q.to_owned().svd(false, false);
-    let effects = get_sum_of_products(&q.transpose(), &inputs);
-    let result = r
-        .qr()
-        .solve(&effects)
-        .ok_or_else(|| err_msg("Solving failed"))?;
-    if result.len() < 2 {
-        bail!("Invalid result matrix");
-    }
-    let intercept = result[0];
-    let slope = result[1];
-    Ok((slope, intercept))
 }
 fn get_sum_of_products(matrix: &DMatrix<f64>, vector: &RowDVector<f64>) -> DMatrix<f64> {
     let mut v: Vec<f64> = Vec::new();
@@ -299,7 +229,6 @@ fn get_sum_of_products(matrix: &DMatrix<f64>, vector: &RowDVector<f64>) -> DMatr
 #[cfg(test)]
 mod tests {
     use super::*;
-    use math::round;
     #[test]
     fn test_pinv_with_formula_builder() {
         use std::collections::HashMap;
@@ -342,49 +271,5 @@ mod tests {
         assert_eq!(regression.rsquared_adj, rsquared_adj);
         assert_eq!(regression.pvalues, pvalues);
         assert_eq!(regression.residuals, residuals);
-    }
-    #[test]
-    fn test_ols_qr() {
-        let inputs = RowDVector::from_vec(vec![1., 3., 4., 5., 2., 3., 4.]);
-        #[rustfmt::skip]
-        let outputs = DMatrix::from_vec(7,2,
-                                        vec![
-                                        1., 1., 1., 1., 1., 1., 1.,
-                                        1., 2., 3., 4., 5., 6., 7.]);
-        let (intercept, slope) = ols_qr(&inputs, &outputs).expect("Solving failed!");
-        let intercept = round::half_up(intercept, 2);
-        let slope = round::half_up(slope, 8);
-        assert_eq!((intercept, slope), (0.25, 2.14285714));
-    }
-    #[test]
-    fn test_ols_pinv_single_regession() {
-        let inputs = RowDVector::from_vec(vec![1., 3., 4., 5., 2., 3., 4.]);
-        #[rustfmt::skip]
-        let outputs = DMatrix::from_vec(7,2,
-                                        vec![
-                                        1., 1., 1., 1., 1., 1., 1.,
-                                        1., 2., 3., 4., 5., 6., 7.]);
-        let params = ols_pinv(&inputs, &outputs).expect("Solving failed!");
-        let intercept = round::half_up(params[0], 8);
-        let slope = round::half_up(params[1], 2);
-        assert_eq!((intercept, slope), (2.14285714, 0.25));
-    }
-    #[test]
-    fn test_ols_pinv_multiple_regression() {
-        let inputs = RowDVector::from_vec(vec![1., 3., 4., 5., 2., 3., 4.]);
-        #[rustfmt::skip]
-        let outputs = DMatrix::from_vec(7,3,
-                                        vec![
-                                        1., 1., 1., 1., 1., 1., 1.,
-                                        1., 2., 3., 4., 5., 6., 7.,
-                                        7., 6., 5., 4., 3., 2., 1.]);
-        let params = ols_pinv(&inputs, &outputs).expect("Solving failed!");
-        let intercept = round::half_up(params[0], 8);
-        let slope = round::half_up(params[1], 8);
-        let slope2 = round::half_up(params[2], 8);
-        assert_eq!(
-            (intercept, slope, slope2),
-            (0.0952381, 0.50595238, 0.25595238)
-        );
     }
 }
